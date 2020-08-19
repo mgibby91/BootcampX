76c2d792eb7c826681e2af7be32fc8a2da8c5a2e
Matthew Gibson <mattgibson6@gmail.com>
2020-08-19 21:36:20 +0000

    sanitized db inputs using escape placeholders from pool dependancy

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,6 +1,7 @@
 const pool = require('./students');
 
 const cohort = process.argv[2];
+const values = [`${cohort}`];
 
 pool.query(`
 SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort
@@ -8,11 +9,11 @@ FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${cohort || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(item => {
       console.log(`${item.cohort}: ${item.teacher}`);
     });
-  });
+  }); 
